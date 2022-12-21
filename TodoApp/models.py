48dcbe7cc52ccb099517c3124ebf16f37722cfd6
MohsenSami <mohsensami88@gmail.com>
2022-12-21 22:46:27 +0330

    Authentication and Authorization

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,20 +1,20 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 from database import Base
 
 
-# class Users(Base):
-#     __tablename__ = "users"
+class Users(Base):
+    __tablename__ = "users"
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     email = Column(String, unique=True, index=True)
-#     username = Column(String, unique=True, index=True)
-#     first_name = Column(String)
-#     last_name = Column(String)
-#     hashed_password = Column(String)
-#     is_active = Column(Boolean, default=True)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
 
-# #     todos = relationship("Todos", back_populates="owner")
+    todos = relationship("Todos", back_populates="owner")
 
 
 class Todos(Base):
@@ -25,6 +25,6 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    # owner_id = Column(Integer, ForeignKey("users.id"))
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    # owner = relationship("Users", back_populates="todos")
+    owner = relationship("Users", back_populates="todos")
